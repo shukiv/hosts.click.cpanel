0a7883f9d6bec35e42170726cbd8e2b3a7d8b6bd
Shuki Vaknin <shukiv@users.noreply.github.com>
2025-01-01 01:57:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-mkdir -p /usr/local/cpanel/base/frontend/jupiter/hosts.click/
 
-cp hostsclick.svg index.live.php install.json /usr/local/cpanel/base/frontend/jupiter/hosts.click/
+if [ "$EUID" -ne 0 ]; then
+    echo "You must run this installation script as root."
+    exit 1
+fi
 
-/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/jupiter/hosts.click/
+INSTALL_DIR="/usr/local/cpanel/base/frontend/jupiter"
+
+if [ ! -d "$INSTALL_DIR" ]; then
+    echo "Installation dir not found: $INSTALL_DIR"
+    exit 1
+fi
+
+echo "Starting hosts.click cPanel Plugin installation..."
+
+mkdir -p $INSTALL_DIR/hosts.click/
+cp hostsclick.svg index.live.php install.json $INSTALL_DIR/hosts.click/
+/usr/local/cpanel/scripts/install_plugin $INSTALL_DIR/hosts.click/
